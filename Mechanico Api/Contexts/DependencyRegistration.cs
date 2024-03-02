e399e633a7cff00fd12737be2d18d829cff51c20
Sajad <sajadrahimi0101@gmail.com>
2024-03-02 09:56:17 +0330

    create class for registers and register sql server

---
+++ Mechanico Api/Contexts/DependencyRegistration.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Mechanico_Api.Contexts;
+
+public abstract class DependencyRegistration
+{
+    public static void RegisterDependencies(WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("mssqlConnection");
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+        
+        builder.Services.AddAutoMapper(typeof(Program));
+
+    }
+}
--- Mechanico Api/Program.cs
+++ Mechanico Api/Program.cs
@@ -1,9 +1,12 @@
+using Mechanico_Api.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+DependencyRegistration.RegisterDependencies(builder);
 
 var app = builder.Build();
 
