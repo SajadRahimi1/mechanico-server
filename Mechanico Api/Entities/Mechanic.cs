74cec940769e61945cf77d9c0dc949103c51c4bf
Sajad <sajadrahimi0101@gmail.com>
2024-03-02 10:14:25 +0330

    created Mechanic Entity

---
+++ Mechanico Api/Entities/Mechanic.cs
@@ -0,0 +1,17 @@
+namespace Mechanico_Api.Entities;
+
+public class Mechanic:BaseEntity
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? StoreName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? CallNumber { get; set; }
+    public string? City { get; set; }
+    public string? Password { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    //TODO: Assign Address
+    public List<Comments> Comments { get; set; } = new();
+    //TODO: Assign Category
+}
