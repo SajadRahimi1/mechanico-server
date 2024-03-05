4fe07907df4b8a64b54fa80c6e0d629d4ae2709c
Sajad <sajadrahimi0101@gmail.com>
2024-03-05 13:25:37 +0330

    test e2e for send code failed

--- TestProject/E2ETest/HttpClientSingleton.cs
+++ TestProject/E2ETest/HttpClientSingleton.cs
@@ -1,6 +1,22 @@
 namespace TestProject.E2ETest;
 
-public class HttpClientSingleton
+public sealed class HttpClientSingleton
 {
-    
+    private static readonly string _baseUrl = "http://localhost:5279";
+    private static HttpClient _httpClient;
+
+    private static HttpClientSingleton _instance=null;
+
+    public static HttpClientSingleton Instance
+    {
+        get { return _instance ??= new HttpClientSingleton(); }
+    }
+
+    private HttpClientSingleton()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public Task<HttpResponseMessage> PostAsync(string url,HttpContent body) => _httpClient.PostAsync($"{_baseUrl}/{url}",body);
+    public Task<HttpResponseMessage> GetAsync(string url) => _httpClient.GetAsync($"{_baseUrl}/{url}");
 }
--- TestProject/E2ETest/TestUsersApi.cs
+++ TestProject/E2ETest/TestUsersApi.cs
@@ -1,6 +1,25 @@
+using System.Net;
+using System.Text;
+using Mechanico_Api.Dtos;
+using Newtonsoft.Json;
+
 namespace TestProject.E2ETest;
 
 public class TestUsersApi
 {
-    
+    private readonly HttpClientSingleton _httpClient;
+
+    public TestUsersApi()
+    {
+        _httpClient = HttpClientSingleton.Instance;
+    }
+
+    [Fact]
+    public async void TestSendCodeWithFailedValidation()
+    {
+        var sendCodeDto = new SendCodeDto { phoneNumber = "sss" };
+        var result = await _httpClient.PostAsync("User/send-code", new StringContent(JsonConvert.SerializeObject(sendCodeDto),Encoding.UTF8,"application/json"));
+        
+        Assert.Equal(HttpStatusCode.BadRequest,result.StatusCode);
+    }
 }
