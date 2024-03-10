590322fa944121c10c0a859de47b7ff5ac512147
Sajad <sajadrahimi0101@gmail.com>
2024-03-10 16:59:06 +0330

    update mechanic api

--- Mechanico Api/Dtos/UpdateMechanicDto.cs
+++ Mechanico Api/Dtos/UpdateMechanicDto.cs
@@ -1,15 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mechanico_Api.Dtos;
 
 public class UpdateMechanicDto
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? StoreName { get; set; }
-    public string? CallNumber { get; set; }
-    
-    public string? City { get; set; }
-    public string? Street { get; set; }
-    public string? Description { get; set; }
-    public double? Latitude { get; set; }
-    public double? Longitude { get; set; }
+    [Required(ErrorMessage = "نام را وارد کنید")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "نام خانوادگی را وارد کنید")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "نام مجموعه را وارد کنید")]
+    public string StoreName { get; set; }
+
+    [Required(ErrorMessage = "شماره تماس را وارد کنید")]
+    public string CallNumber { get; set; }
+
+    [Required(ErrorMessage = "شهر را وارد کنید")]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "خیابان را وارد کنید")]
+    public string Street { get; set; }
+
+    [Required(ErrorMessage = "توضیحات را وارد کنید")]
+    public string Description { get; set; }
+
+    [Range(double.MinValue, double.MaxValue,ErrorMessage = "latitude را وارد کنید")]
+    public double Latitude { get; set; }
+
+    [Range(double.MinValue, double.MaxValue,ErrorMessage = "longitude را وارد کنید")]
+    public double Longitude { get; set; }
 }
